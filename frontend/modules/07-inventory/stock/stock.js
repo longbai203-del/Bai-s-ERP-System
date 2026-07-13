4cd6bc5d419e4993f73e75698d60d57ff44f5f49
baizhanlong <1072360892@qq.com>
2026-07-13 06:31:25 -0700

    Initial commit: Bai's ERP System

---
+++ frontend/modules/07-inventory/stock/stock.js
@@ -0,0 +1,14 @@
+// 07-inventory/stock.js
+console.log('📄 07-inventory/stock page loaded');
+
+export function init() {
+    console.log('07-inventory/stock initialized');
+}
+
+export default {
+    init
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('07-inventory/stock DOM ready');
+});
